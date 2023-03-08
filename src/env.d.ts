a76dd717371a528e1a20a1e24aa72cb882d88866
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-09 03:11:03 +0530

    -: remove spotify for now

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,6 @@
 
 interface ImportMetaEnv {
   readonly DEV_API_KEY: string;
-  readonly SPOTIFY_CLIENT_ID: string;
-  readonly SPOTIFY_CLIENT_SECRET: string;
-  readonly SPOTIFY_REFRESH_TOKEN: string;
 }
 
 interface ImportMeta {
